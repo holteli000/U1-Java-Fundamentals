6df4349df3616402c658c00365aece257b34864d
holteli000 <holteli000@warren.k12.in.us>
2021-08-04 12:53:03 -0400

    completed L1 and L2 Notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,21 @@
-public class L1 {
-
-    //You will notice that the class header is already define.
-
-    //add the main method header below.
-
-
+public class L2{
 }
+    public static void main(String[] args) {
+        double num1 = 3.4;
+        boolean num2 = false;
+        double num3 = 4.5;
+
+        public static void main (String[]args){
+            String firstName = "George";
+            int age = 16;
+            System.out.println(firstName);
+            System.out.println(age);
+
+            String accName = ("elijah");
+            int accNumber = 234565678;
+            double balance = 123456789;
+            System.out.println(accName);
+            System.out.println(accNumber);
+            System.out.println(balance);
+        }
+    }
--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,24 @@
 public class L2 {
 
-    //add the main method header
+    //add thpublic class L2{
+    //}
+    //    public static void main(String[] args) {
+    //        double num1 = 3.4;
+    //        boolean num2 = false;
+    //        double num3 = 4.5;
+    //
+    //        public static void main (String[]args){
+    //            String firstName = "George";
+    //            int age = 16;
+    //            System.out.println(firstName);
+    //            System.out.println(age);
+    //
+    //            String accName = ("elijah");
+    //            int accNumber = 234565678;
+    //            double balance = 123456789;
+    //            System.out.println(accName);
+    //            System.out.println(accNumber);
+    //            System.out.println(balance);
+    //        }
+    //    }e main method header
 }
