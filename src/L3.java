02c0a5ee84a4b4a3b0799fb24d3bf7c5732f20d6
holteli000 <holteli000@warren.k12.in.us>
2021-08-09 12:57:35 -0400

    turn in

--- src/L3.java
+++ src/L3.java
@@ -1,4 +1,43 @@
 public class L3 {
 
-    //add the main method header below.
+   /*
+   literal value is a value written directly into the code
+   An expression is a value written using variables or operators
+
+   A magic number is a literal value that doesnt relate to the program or solution.
+
+   AVOID MAGIC NUMBERS
+    */
+
+    //add the main method header below
+   public static void main(String[] args) {
+       int literalValue= 5;
+       int expression= 5 + 6;
+       int expression2 = literalValue + 6;
+       int expression3 = literalValue + literalValue;
+
+       //Examples of literal values
+       double literalValue2 = 26.4;
+       boolean literalValue3= false;
+       String literalValue4 = "people";
+
+       //examples of expressions - the result of expressions must match the data type
+       double expression4 = 2.4+ 3.6;
+       boolean expression5 = 5 > 6;
+
+       // badExample = 5.4-0.4;
+
+       //String expression is called STRING CONCATENATION. you can combine strings using  the +
+
+       String expression6 = "a movie theater is full of" + literalValue4;
+       System.out.println(expression6);
+
+       String message1 = "The store has ";
+       String message2 = " apples in stock.";
+       int numApples = 20;
+
+       System.out.println("The store has 20 apples in stock.");
+       System.out.println("The store has " + numApples + " apples in stock");
+       System.out.println(message1 + numApples + message2);
+   }
 }
--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,26 @@
 public class L4 {
+
+    public static void main(String[] args) {
+
+        int value= 5;
+
+        // declare a variable - stating a datatype and identifier
+
+        double total;
+        String message = "Hi";
+        final  double GRAVITY = -9.8;
+
+        //assign a value
+        total = 6;
+
+        System.out.println(message + " the value is" + total);
+
+        total = 10;
+        message ="Bye";
+        //GRAVITY = -10;  -- dis is bad
+
+        System.out.println(message + " the value is" + total);
+
+
+    }
 }
