d397bfa8c742df2df87497fd1c8549eb10e25339
holteli000 <holteli000@warren.k12.in.us>
2021-08-19 12:48:28 -0400

    l6 and l7

--- src/L7.java
+++ src/L7.java
@@ -1,8 +1,11 @@
-import java.text.DecimalFormat;  //importing the class from the package
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
+import java.text.DecimalFormat;  //importing the class from the package
+//import java.text.*;
 public class L7{
 
     public static void main(String[] args){
+
 
         //creates a decimal object roundTo2 and initializes it.
         DecimalFormat roundTo2 = new DecimalFormat("0.00");
@@ -23,5 +26,17 @@ public class L7{
 
         //using the roundTo2 object to call the format method from the DecimalFormat class.
         System.out.println("The number rounded to 3 decimals is " + roundTo3.format(number));
+
+        DecimalFormat currency = new DecimalFormat("$#,###.00");
+        System.out.println("the money i have is " + currency.format(number));
+        System.out.println("the money i have is " + currency.format(123456789.5658));
+
+        DecimalFormat percentage = new DecimalFormat("##%");
+        System.out.println("sales tax is: " + percentage.format(0.07));
+
+        DecimalFormat crazy = new DecimalFormat(" THIS IS JUST LOTS OF MONIES $#,###.00");
+        System.out.println(crazy.format (123456789.235694));
+
+
     }
 }
