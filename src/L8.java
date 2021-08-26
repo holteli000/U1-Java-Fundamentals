5ee0b164dc6a969c37a48b6264dc8a95cdc2d573
holteli000 <holteli000@warren.k12.in.us>
2021-08-26 13:15:27 -0400

    lesson 8 and 9

--- src/L8.java
+++ src/L8.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class L8 {
 
     public static void main(String[] args) {
-        String name;
+
+    //Create a Scanner object to read input
+    Scanner read = new Scanner(System.in);
+
+
+
+      /*  String name;
         int hours;
         double payRate;
         double grossPay;
 
-        //Create a Scanner object to read input
-        Scanner read = new Scanner(System.in);
+
 
         //Get the user's name
         System.out.println("What is your name?");
@@ -27,6 +32,36 @@ public class L8 {
         grossPay = hours * payRate;
 
         System.out.println("Hello " + name);
-        System.out.println("Your gross pay is $" + grossPay);
+        System.out.println("Your gross pay is $" + grossPay);*/
+
+        // Bugaboo #1 - hanging carriage
+
+       /* int age;
+        String name;
+
+        System.out.println("how old are you");
+        age = read.nextInt();
+
+        //read.nextLine(); // eats up hanging carriage return
+
+        System.out.println("what is your name");
+        name = read.next();
+
+
+        System.out.println("hello, " + name + ". you are " + age + " years old.");*/
+
+        // Bugaboo #2 - tokens
+
+        String firstName;
+        String lastName;
+
+        System.out.println(" enter you full name ( all on one line, press ENTER when finished) :");
+        firstName = read.next();
+        lastName = read.nextLine();
+
+        System.out.println("hello, " + firstName + " " + lastName);
+
+
+
     }
 }
--- src/L9.java
+++ src/L9.java
@@ -1,3 +1,40 @@
+import javax.swing.JOptionPane;
+
 public class L9{
 
+
+    public static void main(String[] args) {
+
+       String firstName;
+       String lastName;
+
+       firstName = JOptionPane.showInputDialog("what is your first name?");
+       lastName = JOptionPane.showInputDialog("what is your last name?");
+
+
+
+
+
+        JOptionPane.showMessageDialog(null, "hello " + firstName + " " + lastName + "!!");
+
+
+
+        //All input is received as a string
+
+        // pro tip: VARIABLES OF THE SAME DATA-TYPE CAN BE SEPARATED WITH A COMMA IN THE DECLARATION
+
+        int mph, minutes;
+        double distance, hours;
+
+        mph = Integer.parseInt(JOptionPane.showInputDialog("how fast were you travelling (in MPH) ?"));
+        minutes = Integer.parseInt(JOptionPane.showInputDialog("how long, in minutes, were you travelling?"));
+
+        hours = minutes/60.0;
+
+        distance = mph * hours;
+
+        JOptionPane.showMessageDialog(null, "you travelled " + distance + "miles.");
+        System.exit(0);
+
+    }
 }
